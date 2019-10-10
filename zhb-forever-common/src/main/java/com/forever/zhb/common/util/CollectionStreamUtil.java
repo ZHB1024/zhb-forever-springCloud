f94f7eb2489a3b0624e8149d68edab3ecc190644
ZHB1024 <zhb20111503@126.com>
2019-10-10 15:24:31 +0800

    java8 list2map

--- zhb-forever-common/src/main/java/com/forever/zhb/common/util/CollectionStreamUtil.java
+++ zhb-forever-common/src/main/java/com/forever/zhb/common/util/CollectionStreamUtil.java
@@ -2,7 +2,12 @@ package com.forever.zhb.common.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import com.forever.zhb.common.vo.KeyValueVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,16 +16,37 @@ public class CollectionStreamUtil {
     
     
     public static void main(String[] args) {
-        List<String> listString = CollectionUtil.getListString();
-        
-        List<String> result = listString.stream().map(key -> {
-            return key;
+        List<KeyValueVO<String, Integer>> listKeyValueVOs = CollectionUtil.getListKeyValueVO();
+        List<Pair> pairs = listKeyValueVOs.stream().map(vo -> {
+            return Pair.of(vo.getKey(), vo.getValue());
         }).collect(Collectors.toList());
         
-        result.parallelStream().forEach(key -> {
-            pring(key);
-        } );
+        //Map<String, Integer> maps = listKeyValueVOs.stream().collect(Collectors.toMap(KeyValueVO::getKey, KeyValueVO::getValue,(key1,key2)->key1));
+        
+        //Map<String, KeyValueVO> maps = listKeyValueVOs.stream().collect(Collectors.toMap(KeyValueVO::getKey, vo->vo,(key1,key2)->key1));
+        
+        Map<String, List<KeyValueVO<String, Integer>>> maps = listKeyValueVOs.stream().collect(Collectors.groupingBy(KeyValueVO::getKey));
+        
+        
+        maps.forEach((key,value) -> {
+            System.out.println(key + ":" + value);
+        });
+        
+        /*
+         * List<String> listString = CollectionUtil.getListString(); List<String> result
+         * = listString.stream().map(key -> { return key;
+         * }).collect(Collectors.toList()); result.parallelStream().forEach(key -> {
+         * print(key); } );
+         */
+        
+        
+        
     }
+    
+    
+    
+    
+    
     
     private static class CollectionUtil{
         public static List<String> getListString(){
@@ -33,10 +59,22 @@ public class CollectionStreamUtil {
             result.add("6");
             return result;
         }
+        
+        public static List<KeyValueVO<String, Integer>> getListKeyValueVO(){
+            List<KeyValueVO<String, Integer>> result = new ArrayList<KeyValueVO<String,Integer>>();
+            result.add(new KeyValueVO<String, Integer>("1",1));
+            result.add(new KeyValueVO<String, Integer>("1",2));
+            result.add(new KeyValueVO<String, Integer>("1",3));
+            result.add(new KeyValueVO<String, Integer>("2",2));
+            result.add(new KeyValueVO<String, Integer>("3",3));
+            result.add(new KeyValueVO<String, Integer>("4",4));
+            result.add(new KeyValueVO<String, Integer>("5",5));
+            return result;
+        }
     }
     
     
-    public static void pring(String value) {
+    public static void print(String value) {
         System.out.println(value);
     }
     
---
+++ zhb-forever-common/src/main/java/com/forever/zhb/common/vo/KeyValueVO.java
@@ -0,0 +1,36 @@
+package com.forever.zhb.common.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class KeyValueVO<T,V> {
+    
+    private T key;
+    private V value;
+    
+    public KeyValueVO() {
+    }
+    
+    public KeyValueVO(T key,V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+}
